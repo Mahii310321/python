21e9794dabb897f0c129d5100e1cc9b166b15468
Mahii310321 <maheshmahi23798@gmail.com>
2024-04-15 23:41:53 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,50 @@
+# i=1
+# while i <=5:
+#     print(i)
+#     if i==3:
+#         break
+#     i+=1
+    
+# ff
+
+
+
+
+names = ["Dave", "Sara", "John"]
+# for x in names:
+#     print(x)
+
+# for x in "Mississippi":
+#     print(x)
+
+# for x in names:
+#     if x == "Sara":
+#         break
+#     print(x)
+
+# for x in names:
+#     if x == "Sara":
+#         continue
+#     print(x)
+
+# for x in range(4):
+#     print(x)
+
+# for x in range(2, 4):
+#     print(x)
+
+for x in range(5, 101, 5):
+    print(x)
+else:
+    print("Glad that\'s over!")
+
+names = ["Dave", "Sara", "John"]
+actions = ["codes", "eats", "sleeps"]
+
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action + ".")
+
+for action in actions:
+    for name in names:
+        print(name + " " + action + ".")
